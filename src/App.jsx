5a1065860d5f25bfb69f48ba694dc324f7cac92d
PJ Jones <pj.jones4000.com>
2022-02-23 14:53:37 -0600

    centering buttons and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ const App = () => {
 
   useEffect(() => {
     fetch('https://ghibliapi.herokuapp.com/films')
-    .then(res => res.json())
-    .then(allFilms => setFilms(allFilms))
+      .then(res => res.json())
+      .then(allFilms => setFilms(allFilms))
   }, []);
 
   useEffect(() => {
     fetch('https://ghibliapi.herokuapp.com/people')
-    .then(res => res.json())
-    .then(allPeople => setPeople(allPeople))
+      .then(res => res.json())
+      .then(allPeople => setPeople(allPeople))
   }, []);
 
   const handleFilmToggle = () => {
@@ -26,14 +26,14 @@ const App = () => {
   };
   if (loadFilms) {
     return (
-      
+
       <main className='container'>
         <button className='mt-2' onClick={handleFilmToggle} >Back to home</button>
         <section className='row justify-content-center mt-5'>
           {films.map(film => (
             <div className='col-md-6' key={film.id}>
               <div className='card shadow my-2'>
-              <img src={film.image} className="card-img-top" alt="..."/>
+                <img src={film.image} className="card-img-top" alt="..." />
                 <div className='card-body'>
                   <h4 className='card-title'>{film.title}</h4>
                   <p className='card-subtitle text-muted'>{film.original_title}</p>
@@ -57,20 +57,27 @@ const App = () => {
                   <h4 className='card-title'>{person.name}</h4>
                   <p className='card-subtitle text-muted'>{person.gender}</p>
                   <p className="card-text">{person.age}</p>
-                  <a href={person.url}  class="card-link" target="_blank">Link to info</a>
+                  <a href={person.url} class="card-link" target="_blank">Link to info</a>
                 </div>
               </div>
             </div>
           ))}
         </section>
       </main>
-    )} else {
+    )
+  } else {
     return (
-      <div>
-        <h1>Check out this Studio Ghibli stuff!</h1>
-        <button onClick={handleFilmToggle}>Load Films</button>
-        <button onClick={handlePeopleToggle}>Load People</button>
-      </div>
+      <main className='container mt-5'>
+
+        <div className='row justify-content-center'>
+          <h1 className='text-center'>Check out this Studio Ghibli stuff!</h1>
+          <div className='col-md-6 mt-2 text-center'>        <button className='btn-sm' onClick={handleFilmToggle}>Load Films</button>
+          </div>
+          <div className='col-md-6 mt-2 text-center'>
+            <button className='btn-sm' onClick={handlePeopleToggle}>Load People</button>
+          </div>
+        </div>
+      </main>
     )
   }
 }
